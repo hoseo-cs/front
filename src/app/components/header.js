e296b0bbdd50d9f9116995c89a8e81694722ce74
JAE HOON LEE <jazzyrain722@naver.com>
2024-06-19 10:05:13 +0900

    헤더 로딩문제 해결.

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,16 +1,15 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 import SearchBar from "./searchBar";
 
 const Header = () => {
   const router = useRouter();
-  const [mounted, setMounted] = useState(false);
+  //const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  // useEffect(() => {
+  //   setMounted(true);
+  // }, []);
 
   const handleMyPageClick = () => {
     const username = localStorage.getItem("username");
@@ -23,34 +22,32 @@ const Header = () => {
   };
 
   return (
-    mounted && (
-      <div className="w-full h-[80px] flex flex-wrap items-center bg-header-bg p-4 sm:flex-nowrap sm:p-0">
-        {/* <div className="w-full sm:w-auto h-[40px] text-center bg-black text-white flex items-center justify-center mb-2 sm:mb-0 sm:ml-[20px]">
+    <div className="w-full h-[80px] flex flex-wrap items-center bg-header-bg p-4 sm:flex-nowrap sm:p-0">
+      {/* <div className="w-full sm:w-auto h-[40px] text-center bg-black text-white flex items-center justify-center mb-2 sm:mb-0 sm:ml-[20px]">
           LOGO
         </div> */}
-        <img
-          className="w-full sm:w-auto h-[60px] text-center bg-header-bg  text-white flex items-center justify-center mb-2 sm:mb-0 sm:ml-[20px]"
-          src="/assets/minilogo.png"
-        ></img>
-        <div className="w-full sm:w-auto flex justify-around sm:justify-start sm:space-x-4 mb-2 sm:mb-0">
-          <Link className="cursor-pointer" href="/community">
-            커뮤니티
-          </Link>
-          <Link className="cursor-pointer" href="/map">
-            병원 찾기
-          </Link>
-        </div>
-        <div className="w-full sm:w-[340px] flex justify-center sm:mx-auto mb-2 sm:mb-0">
-          <SearchBar />
-        </div>
-        <div
-          className="w-full sm:w-auto flex justify-center sm:justify-end cursor-pointer"
-          onClick={handleMyPageClick}
-        >
-          마이페이지
-        </div>
+      <img
+        className="w-full sm:w-auto h-[60px] text-center bg-header-bg  text-white flex items-center justify-center mb-2 sm:mb-0 sm:ml-[20px]"
+        src="/assets/minilogo.png"
+      ></img>
+      <div className="w-full sm:w-auto flex justify-around sm:justify-start sm:space-x-4 mb-2 sm:mb-0">
+        <Link className="cursor-pointer" href="/community">
+          커뮤니티
+        </Link>
+        <Link className="cursor-pointer" href="/map">
+          병원 찾기
+        </Link>
       </div>
-    )
+      <div className="w-full sm:w-[340px] flex justify-center sm:mx-auto mb-2 sm:mb-0">
+        <SearchBar />
+      </div>
+      <div
+        className="w-full sm:w-auto flex justify-center sm:justify-end cursor-pointer"
+        onClick={handleMyPageClick}
+      >
+        마이페이지
+      </div>
+    </div>
   );
 };
 
