94cc781814db1f048c2350644fba4858e8fc7e3f
JAE HOON LEE <jazzyrain722@naver.com>
2024-06-23 05:24:05 +0900

    랜딩 페이지 모바일 적응형 완료.

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,45 @@
+"use client";
 import Link from "next/link";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+// import required modules
+import { Autoplay, Pagination } from "swiper/modules";
 
 const HomePage = () => {
+  const slideData = [
+    { id: 1, title: "커뮤니티", text: "증상과 성장을 공유" },
+    { id: 2, title: "챗봇", text: "챗봇을 통해 증상 파악" },
+    { id: 3, title: "건강 기록지", text: "기록지로 자세하게 케어" },
+  ];
+
   return (
-    <div className="w-[1340px] h-[630px] mx-auto  flex flex-col lg:flex-row justify-between">
-      <section className="relative flex-1   rounded-xl overflow-hidden">
+    <div className="w-full max-w-[1340px] h-auto lg:h-[620px] mx-auto grid grid-cols-1 lg:grid-cols-5 gap-4 px-4 lg:px-0 overflow-hidden">
+      <section className="relative col-span-1 lg:col-span-4 rounded-xl overflow-hidden h-[300px] lg:h-full shadow-xl">
         <img
-          src="/path-to-image.jpg"
+          src="/assets/landingMap.png"
           alt="Background"
           className="absolute w-full h-full object-cover"
         />
-        <div className="absolute top-1/4 left-1/4 text-white z-10">
-          <h1 className="text-4xl font-bold text-black">특별한 친구를 위한</h1>
-          <h1 className="text-4xl font-bold text-black">특별한 케어 서비스 </h1>
-          <p className="text-5xl font-bold mt-2 text-blue-600">ACARE</p>
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-white p-4 bg-black bg-opacity-50 rounded-lg">
+          <h1 className="text-2xl lg:text-4xl font-bold text-white">
+            특별한 친구를 위한
+          </h1>
+          <h1 className="text-2xl lg:text-4xl font-bold text-white">
+            특별한 케어 서비스
+          </h1>
+          <p className="text-3xl lg:text-5xl font-bold mt-2 text-orange-300">
+            ACARE
+          </p>
         </div>
 
-        <div className="absolute bottom-8 right-2  w-full h-1/3 flex items-end justify-end">
+        <div className="absolute bottom-8 right-2 w-full lg:w-auto h-1/3 flex items-end justify-end p-4 lg:p-0">
           <button className="px-4 py-2 bg-black text-white rounded-full flex items-center space-x-2">
-            <Link href="/map">병원 찾아보기 </Link>
+            <Link href="/map">병원 찾아보기</Link>
             <div className="w-6 h-6 bg-black rounded-full flex items-center justify-center">
               <svg
                 className="w-4 h-4 text-white"
@@ -38,39 +60,47 @@ const HomePage = () => {
         </div>
       </section>
 
-      <aside className="flex flex-col ml-4 space-y-6 mt-6 lg:mt-0 ">
-        <div className="bg-yellow-300 p-4 rounded-lg shadow-md w-48 h-48  flex flex-col justify-between">
+      <aside className="flex flex-col lg:ml-4 space-y-6 mt-6 lg:mt-0 col-span-1 lg:col-span-1 h-auto lg:h-full overflow-y-auto">
+        <div className="bg-yellow-300 p-4 rounded-lg shadow-md w-full h-48 flex flex-col justify-between">
           <p className="text-center text-sm">특수 동물들을 진료하는 병원들</p>
-          <p className="text-center text-2xl font-bold">59,352,891 건</p>
-          <div className="flex justify-center mt-2">
-            <div className="w-2 h-2 bg-black rounded-full mx-1"></div>
-            <div className="w-2 h-2 bg-gray-400 rounded-full mx-1"></div>
-          </div>
+          <p className="text-center text-2xl font-bold my-auto">
+            59,352,891 건
+          </p>
         </div>
 
-        <div className="bg-gray-100 p-4 rounded-lg w-48 h-72 shadow-md  ">
-          <p className="text-center text-sm font-bold bg-black text-white inline-block px-2 py-1 rounded-full">
-            커뮤니티
-          </p>
-          <p className="text-center text-lg font-bold mt-2">
-            증상과 성장을 공유
-          </p>
-          <p className="text-center text-sm text-gray-600 mt-1">
-            #챗봇 #건강한디지털
-          </p>
-          <div className="flex justify-center mt-2">
-            <div className="w-2 h-2 bg-black rounded-full mx-1"></div>
-            <div className="w-2 h-2 bg-gray-400 rounded-full mx-1"></div>
-          </div>
+        <div className="bg-gray-100 p-4 rounded-lg shadow-md w-full h-72">
+          <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Autoplay, Pagination]}
+            className="mySwiper"
+          >
+            {slideData.map((slide) => (
+              <SwiperSlide className="" key={slide.id}>
+                <div className="h-40">
+                  <p className=" text-center text-sm font-bold bg-black text-white inline-block px-2 py-1 rounded-full">
+                    {slide.title}
+                  </p>
+                  <p className=" text-center text-lg font-bold mt-10">
+                    {slide.text}
+                  </p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
 
-        <div className="bg-gray-200 p-4  rounded-lg shadow-md w-48 flex ">
-          <button className="text-2xl">&lt;</button>
+        <div className="bg-gray-200 p-4 rounded-lg shadow-md w-full flex hidden lg:flex">
           <div className="flex-1 mx-2 flex justify-center items-center bg-white rounded-full p-6">
-            {/* <p className="text-lg">github</p> */}
-            <img src="/assets/git.png"></img>
+            <img src="/assets/git.png" alt="Github" />
           </div>
-          <button className="text-2xl">&gt;</button>
         </div>
       </aside>
     </div>
