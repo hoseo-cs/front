6c9f86c0da95c7eac96f9e8dbb395d61dac59eab
JAE HOON LEE <jazzyrain722@naver.com>
2024-07-01 09:57:15 +0900

    랜딩페이지 모바일 수정.

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,7 +1,15 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-
+import { Button } from "../../../@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../../../@/components/ui/dropdown-menu";
 const Header = () => {
   const router = useRouter();
 
@@ -24,7 +32,9 @@ const Header = () => {
           alt="Logo"
         />
       </Link>
-      <div className=" ml-auto flex ">
+      <div className=" ml-auto flex hidden sm:flex ">
+        {/*flex hidden을 함께 사용하면 hidden이 우선 적용되어 요소가 숨겨짐 */}
+        {/* 640px 이상일 때(sm) flex가 나옴-> hidden 클래스가 무시되고 flex가 적용 */}
         <Link className="cursor-pointer mr-3" href="/community">
           커뮤니티
         </Link>
@@ -32,9 +42,40 @@ const Header = () => {
           병원 찾기
         </Link>
         {/* <SearchBar /> */}
-        <div className="cursor-pointer " onClick={handleMyPageClick}>
+        <div className="cursor-pointer mr-4" onClick={handleMyPageClick}>
           마이페이지
         </div>
+      </div>
+      <div className="sm:hidden">
+        {" "}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="rounded-[10px] mr-4">
+              메뉴
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-28 bg-white rounded-[10px]">
+            <DropdownMenuSeparator className="" />
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="flex justify-center items-center h-8">
+                <Link className="cursor-pointer " href="/community">
+                  커뮤니티
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex justify-center items-center h-8">
+                <Link className="cursor-pointer  " href="/map">
+                  병원 찾기
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex justify-center items-center h-8">
+                <div className="cursor-pointer " onClick={handleMyPageClick}>
+                  마이페이지
+                </div>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
